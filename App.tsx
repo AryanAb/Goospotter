a5512d54238ad55e3efd325654822e3b26973c34
AryanAb <aryanabed48@gmail.com>
2021-08-23 17:14:07 -0400

    Added Filter

--- App.tsx
+++ App.tsx
@@ -8,15 +8,29 @@ import {
   Platform,
   PermissionsAndroid,
   TouchableOpacity,
-  Image
+  Image,
+  Button
 } from 'react-native';
 
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
+import Icon from 'react-native-vector-icons/dist/MaterialCommunityIcons';
+import Modal from 'react-native-modal';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const App = () => {
   const [location, setLocation] = useState({ latitude: -1, longitude: -1 });
   const [mapPadding, setMapPadding] = useState(1);
+  const [showFilters, setShowFilters] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "24 hours", value: 24 },
+    { label: "48 hours", value: 48 },
+    { label: "Week", value: 168 },
+    { label: "Don't Show Any", value: -1 }
+  ]);
 
   useEffect(() => {
     const requestPermissions = async () => {
@@ -63,8 +77,8 @@ const App = () => {
       </MapView>
 
       <View style={styles.filterView}>
-        <TouchableOpacity style={{ height: 39, width: 38, backgroundColor: 'rgba(0, 255, 0, 0.6)' }}>
-
+        <TouchableOpacity activeOpacity={0.87} style={styles.filterButton} onPress={() => setShowFilters(true)}>
+          <Icon name='filter' size={25} color='rgba(90, 90, 90, 1.0)' />
         </TouchableOpacity>
       </View>
 
@@ -73,6 +87,29 @@ const App = () => {
           <Image source={require('./assets/Goospotter-logo.png')} style={{ width: 70, height: 70 }} />
         </TouchableOpacity>
       </View>
+
+      <View style={{ flex: 1, height: 10 }}>
+        <Modal isVisible={showFilters} animationIn="fadeIn" animationOut="fadeOut" backdropOpacity={0.6} backdropTransitionOutTiming={0} onBackdropPress={() => setShowFilters(false)}>
+          <View style={{ flex: 0.4, backgroundColor: 'white', padding: 10 }}>
+            <Text style={{ marginLeft: '2.5%', fontSize: 20, marginBottom: 10 }}>Show Spottings in the Past:</Text>
+            <DropDownPicker
+              style={{ width: '90%', marginHorizontal: '5%' }}
+              dropDownContainerStyle={{ width: '90%', marginHorizontal: '5%' }}
+              open={open}
+              value={value}
+              items={items}
+              setOpen={setOpen}
+              setValue={setValue}
+              setItems={setItems}
+            />
+
+            <TouchableOpacity style={styles.OkButton} onPress={() => setShowFilters(false)}>
+              <Text style={{ fontSize: 25 }}>OK</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
+
     </View>
   );
 };
@@ -101,6 +138,24 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: '1.7%',
     right: '87.3%',
+  },
+  filterButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 39,
+    width: 38,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  },
+  OkButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green',
+    width: 100,
+    height: 50,
+    borderRadius: 10,
+    marginTop: '35%',
+    marginLeft: '35%'
   }
 });
 
